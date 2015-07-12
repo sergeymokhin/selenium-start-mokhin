b459a70aafef31b37333d22f4dc1ab3650688b60
sergeymokhin <sergey.mokhin@gmail.com>
2015-07-12 08:25:03 +0600

    add files from selenium start

---
+++ src/test/java/ru/st/selenium/AddMovie.java
@@ -0,0 +1,49 @@
+package ru.st.selenium;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.testng.*;
+import org.testng.annotations.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class AddMovie extends TestBase {
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Test
+  public void testAddMovie() throws Exception {    driver.findElement(By.cssSelector("img[alt=\"Add movie\"]")).click();
+    driver.findElement(By.name("name")).clear();
+    driver.findElement(By.name("name")).sendKeys("Leon");
+    driver.findElement(By.name("year")).clear();
+    driver.findElement(By.name("year")).sendKeys("1994");
+    driver.findElement(By.id("submit")).click();
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
